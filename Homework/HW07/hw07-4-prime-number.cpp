5fb25901aeff53907c7ff8478552a3df06532a43
jirawat malaiwan <tawan.jirawat@gmail.com>
2023-08-16 14:24:07 +0700

    1

--- Homework/HW07/hw07-4-prime-number.cpp
+++ Homework/HW07/hw07-4-prime-number.cpp
@@ -17,19 +17,18 @@
 
 int main()
 {
-    int limit;
-
-    printf("Enter number: ");
-    scanf("%d", &limit);
-
+    int limit ;
     int num = limit;
-    int is_prime = 1; // Assuming num is prime
+    int is_prime = 1 ;
 
+    printf("Enter number: ") ; 
+    scanf("%d", &limit) ;
+    
     do
     {
         if (num <= 1)
         {
-            is_prime = 0;
+            is_prime = 0 ;
         }
         else
         {
@@ -37,22 +36,20 @@ int main()
             {
                 if (num % i == 0)
                 {
-                    is_prime = 0;
-                    break;
+                    is_prime = 0 ;
+                    break ;
                 }
             }
         }
-
         if (is_prime)
         {
-            printf("%d ", num);
+            printf("%d " , num) ;
         }
-
-        num--;
-        is_prime = 1; // Reset is_prime for the next iteration
-    } while (num >= 2);
-
-    printf("\n");
+        num-- ;
+        is_prime = 1 ;
+    } 
+    while (num >= 2) ;
+    printf("\n") ;
 
     return 0;
 }
